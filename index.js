6b98ac0d0bb440a0803011851348e95807bb3408
Sehmer Desktop <matheustae@hotmail.com>
2021-09-03 09:42:17 -0300

    Finished

---
+++ index.js
@@ -0,0 +1,69 @@
+//console.log(await Promise.resolve(true))
+
+import { dirname, join } from "path";
+import { promisify } from "util";
+import { promises, createReadStream, createWriteStream } from "fs";
+import { pipeline, Transform } from "stream";
+const pipelineAsync = promisify(pipeline);
+const { readdir } = promises;
+
+import csvtojson from 'csvtojson'
+import jsontocsv from 'json-to-csv-stream'
+
+import StreamConcat from 'stream-concat'
+
+
+import debug from "debug";
+const log = debug("app:concat");
+
+//Garantindo que independentemente de onde for rodado
+// consiguira rodar da forma correta
+const { pathname: currentFile } = new URL(import.meta.url);
+const cwd = dirname(currentFile);
+const filesDir = `${cwd}/dataset`;
+const output = `${cwd}/final.csv`;
+
+console.time("concat-data");
+
+const files = (await readdir(filesDir)).filter(
+    (item) => !!!~item.indexOf(".zip")
+);
+
+log(`processing: ${files}`);
+const ONE_SECOND = 1000;
+
+
+//Quando os outros processos acabarem, ele morre junto
+setInterval(() => process.stdout.write("."), ONE_SECOND).unref();
+
+//const combinedStreams = createReadStream(join(filesDir, files[0]));
+const streams = files.map(
+    item => createReadStream(join(filesDir, item)),
+)
+const combinedStreams = new StreamConcat(streams)
+const finalStream = createWriteStream(output);
+
+const handleStream = new Transform({
+    transform: (chunk, encoding, cb) => {
+        const data = JSON.parse(chunk);
+        const output = {
+            id: data.Respondent,
+            country: data.Country
+        }
+
+       log(`id: ${output.id}`)
+        return cb(null, JSON.stringify(output));
+    },
+});
+
+await pipelineAsync(
+    combinedStreams, 
+    csvtojson(),
+    handleStream, 
+    jsontocsv(),
+    finalStream
+    );
+
+log(`${files.length} files merged on ${output}`);
+
+console.timeEnd("concat-data");
